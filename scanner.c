114bdd26f34ed1be5557dc516ed3f9f868c8478e
TioStitch <87840489+TioStitch@users.noreply.github.com>
2024-07-12 22:06:13 -0300

    Update scanner.c

--- scanner.c
+++ scanner.c
@@ -1,40 +1,39 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <stdbool.h>
 #include <sys/types.h>
 
 int nextInt() {
-    int *i = (int *) malloc((sizeof(int)));
-    scanf("%d", i);
-    return *i;
+    int i = 0;
+    scanf("%d", &i);
+    return i;
 }
 
 float nextFloat() {
-    float *i = (float *) malloc((sizeof(float)));
-    scanf("%f", i);
-    return *i;
+    float i = 0;
+    scanf("%f", &i);
+    return i;
 }
 
 double nextDouble() {
-    double *i = (double *) malloc((sizeof(double)));
-    scanf("%lf", i);
-    return *i;
+    double i = 0;
+    scanf("%lf", &i);
+    return i;
 }
 
 bool nextBoolean() {
-    bool *i = (bool *) malloc((sizeof(bool)));
-    scanf("%c", i);
-    return *i;
+    bool i = 0;
+    scanf("%c", &i);
+    return i;
 }
 
 short nextShort() {
-    short *i = (short *) malloc((sizeof(short)));
-    scanf("%hi", i);
-    return *i;
+    short i = 0;
+    scanf("%hi", &i);
+    return i;
 }
 
 long nextLong() {
-    long *i = (long *) malloc((sizeof(long)));
-    scanf("%ld", i);
-    return *i;
+    long i = 0;
+    scanf("%ld", &i);
+    return i;
 }
